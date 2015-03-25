d55829c8ef66b99939cb6873985249a282e15a21
Derrick Fountain <djfj74@icloud.com>
2015-03-24 21:20:45 -0400

    [WIP] 03.24.2015

--- openos/startup/openos-boot.lua
+++ openos/startup/openos-boot.lua
@@ -1,9 +1,5 @@
---OpenOS Startup File
-print("Loading APIs...")
+
 os.loadAPI("/lib/libde")
 os.loadAPI("/lib/libtext")
 os.loadAPI("/lib/libget")
-print("Checking for updates...")
-shell.run("ac-get", "update")
-print("Starting OpenOS...")
 shell.run("/bin/desktop")
