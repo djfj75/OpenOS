545e29ea7c91c188d1670f6650af87628324a7a0
Derrick Fountain <djfj74@icloud.com>
2015-03-27 19:39:46 -0400

    no message

--- appstore/bin/appstore.lua
+++
@@ -1,9 +0,0 @@
---OpenOS Appstore
-
-term.setBackgroundColor(colors.lightGray)
-term.clear()
-term.setBackgroundColor(colors.blue)
-term.setTextColor(colors.black)
-term.clearLine(1)
-term.setCursorPos(1,1)
-print("OpenOS Appstore")
